52ae7fd7dd446753fecf84b24f657f61d7b13df9
Jude Dike <dikejude49@gmail.com>
2021-03-18 16:24:34 +0100

    refactored for main use

--- scripts/generate-merkle-root.ts
+++ scripts/generate-merkle-root.ts
@@ -1,18 +1,18 @@
-import { program } from 'commander'
+// import { program } from 'commander'
 import fs from 'fs'
 import { parseBalanceMap } from '../src/parse-balance-map'
 
-program
-  .version('0.0.0')
-  .requiredOption(
-    '-i, --input <path>',
-    'input JSON file location containing a map of account addresses to string balances'
-  )
+// program
+//   .version('0.0.0')
+//   .requiredOption(
+//     '-i, --input <path>',
+//     'input JSON file location containing a map of account addresses to string balances'
+//   )
 
-program.parse(process.argv)
+// program.parse(process.argv)
 
-const json = JSON.parse(fs.readFileSync(program.input, { encoding: 'utf8' }))
+const json = JSON.parse(fs.readFileSync("./scripts/example.json", { encoding: 'utf8' }))
 
 if (typeof json !== 'object') throw new Error('Invalid JSON')
-
-console.log(JSON.stringify(parseBalanceMap(json)))
+fs.writeFileSync("./scripts/claims.json", JSON.stringify(parseBalanceMap(json)), { encoding: 'utf8' })
+// console.log(JSON.stringify(parseBalanceMap(json)))
